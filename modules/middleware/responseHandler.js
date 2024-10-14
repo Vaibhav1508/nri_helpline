ad1513d492efd24c6166aff40e7efaec924736db
Vibhu@1508 <vaibhavmehta974@gmail.com>
2024-10-14 22:06:24 +0530

    changes in response handler

--- modules/middleware/responseHandler.js
+++ modules/middleware/responseHandler.js
@@ -1,7 +1,97 @@
-"use strict";
+// "use strict";
+
+// /**
+//  * Middleware to intercept success and error reponses.
+//  */
+// let _ = require("lodash");
+
+// /**
+//  * Custom errors
+//  */
+// let BadRequestError = require("../errors/badRequestError"),
+//   AccessDeniedError = require("../errors/accessDeniedError"),
+//   UnauthorizedError = require("../errors/unauthorizedError"),
+//   EntityNotFoundError = require("../errors/entityNotFoundError");
+
+// const HTTP_STATUS = require("../constants/httpStatus");
+// const AUTH_LENGTH = 8;
+
+// /**
+//  * Listens to success of the response.
+//  * This should be the first middleware for the request.
+//  */
+// let onSuccess = (req, res, next) => {
+//   req._startTime = Date.now();
+
+//   /**
+//    * res.end is the method which is called at the end of the request.
+//    * We overwrite this method with our own so that we can intercept the final response for logging.
+//    */
+//   let end = res.end;
+//   res.end = (chunk, encoding) => {
+//     /**
+//      * Restore the original end function
+//      */
+//     res.end = end;
+//     /**
+//      * Call the original end function
+//      */
+//     res.end(chunk, encoding);
+//   };
+
+//   return next();
+// };
+
+// /**
+//  * Handles error responses.
+//  */
+// let onError = (err, req, res, next) => {
+//   //console.log('Inside error listener.',err);
+
+//   // if (err.message === "Only images are allowed") {
+//   //   res.status(500).json({
+//   //     status: "error",
+//   //     message: "Only images are allowed",
+//   //   });
+
+//   res.status(err.status || HTTP_STATUS.INTERNAL_SERVER_ERROR);
+
+//   /**
+//    * Handle known errors first.
+//    */
+//   if (
+//     err instanceof AccessDeniedError ||
+//     err instanceof BadRequestError ||
+//     err instanceof UnauthorizedError ||
+//     err instanceof EntityNotFoundError
+//   ) {
+//     res.json(err.json);
+
+//     return next();
+//   }
+
+//   /**
+//    * Handle internal server errors.
+//    */
+//   let response = {
+//     message: "Internal server error.",
+//     status: -1,
+//   };
+//   res.json(response);
+
+//   return next(err);
+// };
+
+// module.exports = {
+//   onSuccess: onSuccess,
+//   onError: onError,
+// };
+
+
+'use strict';
 
 /**
- * Middleware to intercept success and error reponses.
+ * Middleware to intercept success and error responses.
  */
 let _ = require("lodash");
 
@@ -14,7 +104,6 @@ let BadRequestError = require("../errors/badRequestError"),
   EntityNotFoundError = require("../errors/entityNotFoundError");
 
 const HTTP_STATUS = require("../constants/httpStatus");
-const AUTH_LENGTH = 8;
 
 /**
  * Listens to success of the response.
@@ -46,13 +135,7 @@ let onSuccess = (req, res, next) => {
  * Handles error responses.
  */
 let onError = (err, req, res, next) => {
-  //console.log('Inside error listener.',err);
-
-  // if (err.message === "Only images are allowed") {
-  //   res.status(500).json({
-  //     status: "error",
-  //     message: "Only images are allowed",
-  //   });
+  console.error('Error caught in error handler:', err);  // Log the error for debugging
 
   res.status(err.status || HTTP_STATUS.INTERNAL_SERVER_ERROR);
 
@@ -65,24 +148,25 @@ let onError = (err, req, res, next) => {
     err instanceof UnauthorizedError ||
     err instanceof EntityNotFoundError
   ) {
-    res.json(err.json);
-
-    return next();
+    return res.json(err.json);  // Send the specific error response
   }
 
   /**
-   * Handle internal server errors.
+   * Handle unknown internal server errors.
    */
   let response = {
     message: "Internal server error.",
     status: -1,
+    error: err.message || 'Unknown error'  // Add error message to help identify issues
   };
+
   res.json(response);
 
-  return next(err);
+  return next(err);  // Pass the error to the next middleware if needed
 };
 
 module.exports = {
   onSuccess: onSuccess,
   onError: onError,
 };
+
