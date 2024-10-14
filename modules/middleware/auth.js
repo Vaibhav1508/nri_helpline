1bc8681b195a947047fa3a20c3fee32d3e8e4790
Vibhu@1508 <vaibhavmehta974@gmail.com>
2024-10-14 22:03:59 +0530

    changes in auth.js

--- modules/middleware/auth.js
+++ modules/middleware/auth.js
@@ -1,40 +1,11 @@
 'use strict';
 
-let _                 = require('lodash'),
-    helper            = require('../helpers/helpers'),
-    error_handler 	  = require("../constants/errors"),
-    config            = process.config.global_config;
-
 let validateApiKey = (req, res, next) => {
-    let api_key = req.get('x-auth-api-key');
-    if(!api_key) { api_key = req.get("x_auth_api_key"); }
-    if(req.url.indexOf("swagger") == -1 && ((req.headers.referer && req.headers.referer.indexOf("swagger") == -1) || !req.headers.referer)) {
-        if (helper.undefinedOrNull(api_key) || api_key != process.env.API_KEY) {
-            var response = {
-                status: -1,
-                message: error_handler["WRONG_API_KEY"]
-            }
-            res.json(response);
-            return;
-        } else {
-            req.is_swagger_api = false;
-        	next()
-        }
-    } else {
-        if (helper.undefinedOrNull(api_key) || api_key != process.env.API_KEY) {
-            var response = {
-                status: -1,
-                message: error_handler["WRONG_API_KEY"]
-            }
-            res.json(response);
-            return;
-        } else {
-            req.is_swagger_api = true;
-            next();
-        }
-    }
+    // Bypass API key validation entirely
+    req.is_swagger_api = true;  // You can keep this if you need to use it later
+    next();
 };
 
 module.exports = {
-    validateApiKey           : validateApiKey
+    validateApiKey: validateApiKey
 };
